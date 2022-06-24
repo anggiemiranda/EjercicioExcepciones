f8a8f050aa6beb4203cf5a4cc71bdbd4855e4212
anggiemiranda <amirandahuallpa@santandertecnologia.com.ar>
2022-06-24 02:33:59 -0300

    spring initializr

---
+++ Ejercicio-Excepciones/src/test/java/com/example/EjercicioExcepciones/EjercicioExcepcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioExcepciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioExcepcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
